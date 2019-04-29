439cc76f62421e19d3d65c74e0374ce8205f86e2
statnmap <statnmap@users.noreply.github.com>
2019-04-29 15:18:22 +0200

    include pkgdown in package

--- R/dr_help.R
+++ R/dr_help.R
@@ -1,8 +1,11 @@
 #' Ouvrir la page d'aide
 #' @export
 #' @importFrom utils browseURL
-dr_help <- function(){
-  browseURL(
-    system.file("docs", "index.html", package = "shinydreal")
-  )
+dr_help <- function() {
+  guide_path <- system.file('docs/index.html', package = 'shinydreal')
+  if (guide_path == "") {
+    stop('There is no pkgdown site in ', 'docs/index.html')
+  }
+  
+  browseURL(paste0('file://', guide_path))
 }
--- dev.R
+++ dev.R
@@ -11,3 +11,27 @@ visualidentity::build_pkgdown(
   favicon = system.file("pkgdown/favicon.ico", package = "thinkridentity"),
   move = TRUE, clean_before = TRUE, clean_after = TRUE
 )
+
+
+# visualidentity::open_pkgdown_function(path = "inst/docs")
+# pkg::open_pkgdown()
+
+## __ deploy on rsconnect
+usethis::use_git_ignore("docs/rsconnect")
+usethis::use_git_ignore("inst/docs/rsconnect")
+usethis::use_git_ignore("rsconnect")
+
+rsconnect::accounts()
+account_name <- rstudioapi::showPrompt("Rsconnect account", "Please enter your username:", "name")
+account_server <- rstudioapi::showPrompt("Rsconnect server", "Please enter your server name:", "1.1.1.1")
+origwd <- setwd("inst/docs")
+rsconnect::deployApp(
+  ".",                       # the directory containing the content
+  appFiles = list.files(".", recursive = TRUE), # the list of files to include as dependencies (all of them)
+  appPrimaryDoc = "index.html",                 # the primary file
+  appName = "shinydreal",                   # name of the endpoint (unique to your account on Connect)
+  appTitle = "shinydreal",                  # display name for the content
+  account = account_name,                # your Connect username
+  server = account_server                    # the Connect server, see rsconnect::accounts()
+)
+setwd(origwd)
