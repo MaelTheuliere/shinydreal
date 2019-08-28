d666d56d0a936f22643b12d49af021174947a7f6
statnmap <statnmap@users.noreply.github.com>
2019-08-28 14:06:02 +0200

    selectizeInput css

--- inst/shinydashboard/app.R
+++ inst/shinydashboard/app.R
@@ -2,6 +2,7 @@ library(shinydashboard)
 library(shiny)
 library(shinydreal)
 library(shinipsum)
+library(htmltools)
 
 skin <- Sys.getenv("DASHBOARD_SKIN")
 skin <- tolower(skin)
@@ -171,6 +172,8 @@ body <- dashboardBody(
           varSelectInput("variabledb", "varSelectInput", mtcars),
           selectInput("n_breaks", label = "selectInput with multiple vars",
                       choices = c(10, 20, 35, 50, 100), selected = 20, multiple = TRUE),
+          # selectizeInput("selectize", label = "selectizeInput with multiple vars",
+                      # choices = c(10, 20, 35, 50, 100), selected = 20, multiple = TRUE),
           sliderInput("obsdb", "sliderInput",
                       min = 0, max = 1000, value = 500
           )
